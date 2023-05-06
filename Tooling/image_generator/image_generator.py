562091f1859f2e544f137ae5a9e6223ef680746f
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-05-07 00:24:09 +0200

    Added coordinate retrieval function for LED coordinates.

--- Tooling/image_generator/image_generator.py
+++ Tooling/image_generator/image_generator.py
@@ -1,72 +1,147 @@
-import tkinter as tk
 from tkinter import *
 from tkinter import colorchooser
 from PIL import ImageTk
 from tkinter.colorchooser import *
+import os
 
-# Define a function to change the color of the selected circles
-def change_color(color):
-    for led in selected_leds:
-        canvas.itemconfig(led, fill=color)
+class ImageGenerator:
+    def __init__(self):
+        
+        self.tooltip_text = ""
+        self.file_path = "coordinates.txt"
+        self.index = 0
+        
+        # Create a Tkinter window
+        self.root = Tk()
+        # Load an image using Pillow
+        self.image = ImageTk.PhotoImage(file="./Disco-Neut.jpg")
+        
+        # Create a canvas to display the image and circles
+        self.canvas = Canvas(self.root, width=self.image.width(), height=self.image.height())
+        self.canvas.pack()
 
-# Define a function to handle circle selection
-def select_led(event):
+        # Display the image on the canvas
+        self.canvas.create_image(0, 0, anchor="nw", image=self.image)
+        
+        # Create the tooltip
+        self.create_tooltip()
+        
+        # Get canvas heigth
+        self.heigth = self.canvas.winfo_height()
+        
+        
+        # Add a binding to the canvas to show and hide the tooltip
+        self.canvas.bind('<Motion>', self.show_tooltip)
+        self.canvas.bind('<Leave>', self.hide_tooltip)
+        
+        # Add a binding to the canvas to export coordinates on spacebar press
+        self.root.bind('<space>', self.export_coordinates)
+        
+        # Draw circles on top of the image
+        self.led1 = self.create_led(100, 100)
+        self.led2 = self.create_led(150, 150)
+        self.led3 = self.create_led(200, 200)
+
+        # Bind a function to the circles so that they can be selected
+        self.canvas.tag_bind(self.led1, "<Button-1>", self.select_led)
+        self.canvas.tag_bind(self.led2, "<Button-1>", self.select_led)
+        self.canvas.tag_bind(self.led3, "<Button-1>", self.select_led)
+
+        # Start the Tkinter event loop
+        self.root.mainloop()    
     
-    #Get tag of the clicked circle
-    led_tags = canvas.gettags("current")
+
+    def create_tooltip(self):
+        # Create tooltip window
+        self.tip = Toplevel(self.root)
+        self.tip.withdraw()
+        self.tip.overrideredirect(True)
+        self.tip.geometry("+{x}+{y}".format(x=0, y=self.root.winfo_height()))
+        
+        # Create label for tooltip text
+        self.tip_label = Label(self.tip, text=self.tooltip_text, bg='white', padx=5, pady=2)
+        self.tip_label.pack()
+        
+    def show_tooltip(self, event):
+        # Update tooltip text and position
+        self.tooltip_text = f"X: {event.x}, Y: {event.y}"
+        self.tip_label.config(text=self.tooltip_text)
+        self.tip.geometry("+{x}+{y}".format(x=event.x_root+10, y=event.y_root+10))
+        
+        # Show tooltip
+        self.tip.deiconify()
     
-    print("Current LED tag: ", led_tags)
+    def hide_tooltip(self, event):
+        # Hide tooltip
+        self.tip.withdraw()
+
+
+    # Define a function to change the color of the selected led
+    def change_color(self, color):
+        for led in self.selected_leds:
+            self.canvas.itemconfig(led, fill=color)
+
+    # Define a function to handle led selection
+    def select_led(self, event):
+        
+        #Get tag of the clicked circle
+        led_tags = self.canvas.gettags("current")
+        
+        print("Current LED tag: ", led_tags)
+        
+        if len(led_tags) > 0:
+            led_tag = led_tags[0]
+            self.canvas.itemconfig(led_tag, width = 3)
+            color = askcolor()[1]
+            self.canvas.itemconfig(led_tag, fill = color)
+        else:
+            print("Failed to get LED tag")
+        
+    def create_led(self, xPos, yPos):
+        lRadius = 20
+        lColor = "Black"
+        led = self.canvas.create_oval( xPos - lRadius, yPos - lRadius, xPos + lRadius, yPos + lRadius, fill = lColor )
+        self.canvas.itemconfig(led, tags =(str(led), lColor))
+        print(self.canvas.gettags(led))
+        return led
     
-    if len(led_tags) > 0:
-        led_tag = led_tags[0]
-        canvas.itemconfig(led_tag, width = 3)
-        color = askcolor()[1]
-        canvas.itemconfig(led_tag, fill = color)
-    else:
-        print("Failed to get LED tag")
+    # Create the coordinates file if it does not exist
+    def create_coordinates_file(self):
+        if not os.path.exists('coordinates.txt'):
+            print("Coordinates file not found, creating now")
+            open(self.file_path, 'w').close()
+        else:
+            print("Coordinates file found, adding new coordinates")
     
-def create_led(xPos, yPos):
-    lRadius = 20
-    lColor = "Black"
-    led = canvas.create_oval( xPos - lRadius, yPos - lRadius, xPos + lRadius, yPos + lRadius, fill = lColor )
-    canvas.itemconfig(led, tags =(str(led), lColor))
-    print(canvas.gettags(led))
-    return led
+    def export_coordinates(self, event):
+        
+        
+             
+        self.create_coordinates_file()
+        
+        print("export coordinates:")
+        # Export the coordinates of the current mouse pointer
+                
+        x = event.x
+        y = event.y
+        
+        with open(self.file_path, "r") as f:
+            
+            #Get the number of lines in the file
+            linesInFile = len(f.readlines())
+            
+            print("Lines in file: " + str(linesInFile))
+            print("Index = " + str(self.index))
 
-# Create a Tkinter window
-root = tk.Tk()
-
-# Load an image using Pillow
-image = ImageTk.PhotoImage(file="./Disco-Neut.jpg")
-
-# Create a canvas to display the image and circles
-canvas = tk.Canvas(root, width=image.width(), height=image.height())
-canvas.pack()
-
-# Display the image on the canvas
-canvas.create_image(0, 0, anchor="nw", image=image)
-
-# Draw circles on top of the image
-led1 = create_led(100, 100)
-led2 = create_led(150, 150)
-led3 = create_led(200, 200)
-
-# Bind a function to the circles so that they can be selected
-canvas.tag_bind(led1, "<Button-1>", select_led)
-canvas.tag_bind(led2, "<Button-1>", select_led)
-canvas.tag_bind(led3, "<Button-1>", select_led)
-
-# Create a color palette
-palette = tk.Frame(root)
-palette.pack()
-
-colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "black"]
-for color in colors:
-    button = tk.Button(palette, bg=color, width=3, command=lambda c=color: change_color(c))
-    button.pack(side="left")
-
-# Create a list to keep track of selected circles
-selected_leds = []
-
-# Start the Tkinter event loop
-root.mainloop()
+        with open(self.file_path, 'a') as f:                
+            stringToPrint = str(self.index) + '; X: ' +  str(x) + ' ; Y: ' + str(y)+ '\n'
+            print("Writing to file -> " + stringToPrint)
+            f.write(stringToPrint)
+            f.close()
+            
+        self.index = self.index + 1
+            
+                
+if __name__ == "__main__":
+    ImageGenerator()
+    
