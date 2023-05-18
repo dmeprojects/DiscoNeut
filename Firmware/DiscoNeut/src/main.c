a5e981db70b2e40bb94c47e8573b6ac89785a761
DME-Projects <79807855+dmeprojects@users.noreply.github.com>
2023-05-19 01:23:17 +0200

    Added wifi drivers.  This affects the reading of the buffers

--- Firmware/DiscoNeut/src/main.c
+++ Firmware/DiscoNeut/src/main.c
@@ -1,6 +1,7 @@
 /*Standard libraries*/
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
 #include "inttypes.h"
 
@@ -8,12 +9,35 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+#include "freertos/semphr.h"
 
 
 /*ESP-IDF libraries*/
 #include "esp_log.h"
 #include "led_strip.h"
 #include "sdkconfig.h"
+
+/* #include "usb/cdc_acm_host.h"
+#include "usb/vcp_ch34x.hpp"
+#include "usb/vcp_cp210x.hpp"
+#include "usb/vcp_ftdi.hpp"
+#include "usb/vcp.hpp"
+#include "usb/usb_host.h"
+
+using namespace esp_usb;
+*/
+
+#include "esp_system.h"
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+
 
 /*System perhiperhals*/
 #include "driver/i2s_std.h"
@@ -44,7 +68,12 @@ static const char *TAG = "main";
 //#define NOISELEVEL      5000
 
 /*MIC BUFFER DEFINES*/
-#define MIC_BUFFER_SIZE 32   //Default 128
+#define MIC_BUFFER_SIZE 3   //Default 128
+
+#define EXAMPLE_ESP_WIFI_SSID "Disconeut"
+#define EXAMPLE_ESP_WIFI_PASS "DsicoNeut"
+#define EXAMPLE_ESP_WIFI_CHANNEL   6
+#define EXAMPLE_MAX_STA_CONN (3)
 
 /*LED VARIABLES*/
 led_strip_handle_t led_strip;
@@ -92,6 +121,70 @@ i2s_std_config_t i2s_config =
 TaskHandle_t audioReceiveTaskHandle = NULL;
 
 uint8_t lLed = 0;
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                              int32_t event_id, void *event_data)
+{
+  if (event_id == WIFI_EVENT_AP_STACONNECTED)
+  {
+    wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
+    ESP_LOGI(TAG, "station " MACSTR " join, AID=%d",
+            MAC2STR(event->mac), event->aid);
+  }
+  else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
+  {
+    wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
+    ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d",
+            MAC2STR(event->mac), event->aid);
+  }
+}
+
+void wifi_init_softap()
+{
+     ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .ssid_len = strlen(EXAMPLE_ESP_WIFI_SSID),
+            .channel = EXAMPLE_ESP_WIFI_CHANNEL,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            .max_connection = EXAMPLE_MAX_STA_CONN,
+#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+            .authmode = WIFI_AUTH_WPA3_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+            .authmode = WIFI_AUTH_WPA2_PSK,
+#endif
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    if (strlen(EXAMPLE_ESP_WIFI_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
+             EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
+}
+
+
 
 static void initLedPower( void )
 {
@@ -353,6 +446,19 @@ void app_main()
     BaseType_t lStatus = pdFALSE;
     uint8_t loopCounter = 0;
     uint8_t i = 0;
+
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    wifi_init_softap();
+
+
 
     /* Configure the peripheral according to the LED type */
     configure_led();
